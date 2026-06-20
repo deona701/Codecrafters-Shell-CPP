d8e91c2e706c71df5c192f3d57977bf48a840419
deona701 <deonaleard@gmail.com>
2026-06-20 12:00:27 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,13 @@ int main() {
     std::cout << "$ ";
     std::string command;
     std::getline(std::cin, command);
-    std::cout << command << ": command not found" << std::endl; 
 
     if (command == "exit") {
       break;
+    }
+
+    else {
+      std::cout << command << ": command not found" << std::endl; 
     }
   }
 
