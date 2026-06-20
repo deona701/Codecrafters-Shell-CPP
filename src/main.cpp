4530a6e9bb67edacf2d72f6becb3351fe2f7b766
deona701 <deonaleard@gmail.com>
2026-06-20 13:51:14 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,48 @@
 #include <iostream>
 #include <string>
 #include <stdbool.h>
+#include <vector>
+using namespace std;
 
 int main() {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  cout << unitbuf;
+  cerr << unitbuf;
 
 
   while (true) {
-    std::cout << "$ ";
-    std::string command;
-    std::getline(std::cin, command);
+    cout << "$ ";
+    string command;
+    getline(cin, command);
+    vector<string> commands = {"exit", "echo", "type"};
 
     if (command == "exit") {
       break;
     }
 
-    if (command.substr(0, 5) == "echo ") {
-      std::cout << command.substr(5) << std::endl;
+    else if (command.substr(0, 5) == "echo ") {
+      cout << command.substr(5) << endl;
     }
 
+    else if (command.substr(0, 5) == "type ") {
+    string requested_command = command.substr(5);
+    bool found = false;
+
+    for (string builtin : commands) {
+      if (requested_command == builtin) {
+        cout << builtin << " is a shell builtin" << endl;
+        found = true;
+        break; 
+      }
+    }
+
+    if (!found) {
+      cout << requested_command << ": not found" << endl;
+    }
+  }
+
     else {
-      std::cout << command << ": command not found" << std::endl; 
+      cout << command << ": command not found" << endl; 
     }
   }
 
