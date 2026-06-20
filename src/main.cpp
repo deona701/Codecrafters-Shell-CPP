4336c42191627e200c2d0e2434ded61671443d85
deona701 <deonaleard@gmail.com>
2026-06-20 16:15:31 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,68 @@
 #include <iostream>
 #include <string>
-#include <stdbool.h>
 #include <vector>
+#include <sstream>   
+#include <cstdlib>   
+#include <unistd.h> 
+
 using namespace std;
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  cout << unitbuf;
-  cerr << unitbuf;
+    // Flush after every std::cout / std::cerr
+    cout << unitbuf;
+    cerr << unitbuf;
 
-
-  while (true) {
-    cout << "$ ";
-    string command;
-    getline(cin, command);
     vector<string> commands = {"exit", "echo", "type"};
 
-    if (command == "exit") {
-      break;
+    while (true) {
+        cout << "$ ";
+        string command;
+        getline(cin, command);
+
+        if (command == "exit") {
+            break;
+        }
+       
+        else if (command.size() >= 5 && command.substr(0, 5) == "echo ") {
+            cout << command.substr(5) << endl;
+        }
+        else if (command.size() >= 5 && command.substr(0, 5) == "type ") {
+            string requested_command = command.substr(5);
+            bool found = false;
+
+            for (string builtin : commands) {
+                if (requested_command == builtin) {
+                    cout << builtin << " is a shell builtin" << endl;
+                    found = true;
+                    break; 
+                }
+            }
+
+            if (found == false) {
+                string path = getenv("PATH");
+                stringstream ss(path);
+                string directory;
+                bool binary_found = false;
+
+                while (getline(ss, directory, ':')) {
+                    string full_path = directory + "/" + requested_command;
+                    if (!access(full_path.c_str(), X_OK)) {
+                        cout << requested_command << " is " << full_path << endl;
+                        binary_found = true;
+                        break; 
+                    }
+                }
+
+                
+                if (!binary_found) {
+                    cout << requested_command << ": not found" << endl;
+                }
+            }
+        } 
+        else {
+            cout << command << ": command not found" << endl; 
+        }
     }
 
-    else if (command.substr(0, 5) == "echo ") {
-      cout << command.substr(5) << endl;
-    }
-
-    else if (command.substr(0, 5) == "type ") {
-    string requested_command = command.substr(5);
-    bool found = false;
-
-    for (string builtin : commands) {
-      if (requested_command == builtin) {
-        cout << builtin << " is a shell builtin" << endl;
-        found = true;
-        break; 
-      }
-    }
-
-    if (!found) {
-      cout << requested_command << ": not found" << endl;
-    }
-  }
-
-    else {
-      cout << command << ": command not found" << endl; 
-    }
-  }
-
+    return 0;
 }
